a7b0abf6a68403616de366bd0bc8ab97e8072238
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-03-31 17:43:34 +0200

    chore(package): Add benchmarks

---
+++ benchmark/index.js
@@ -0,0 +1,105 @@
+var bench = require( 'nanobench' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+var MBR = require( '..' )
+
+const ITERATIONS = 10000
+
+bench( `MBR.detectFormat() ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'syslinux.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.detectFormat(buffer)
+  }
+  run.end()
+
+})
+
+bench( `MBR#getEFIPart() ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'bootcamp-hybrid.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = MBR.parse(buffer)
+  var efiPart = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    efiPart = mbr.getEFIPart()
+  }
+  run.end()
+
+})
+
+bench( `MBR.parse(rpi3) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'rpi3.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.parse(buffer)
+  }
+  run.end()
+
+})
+
+bench( `MBR.parse(rufus) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'rufus.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.parse(buffer)
+  }
+  run.end()
+
+})
+
+bench( `MBR.parse(bootcamp) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'bootcamp-hybrid.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.parse(buffer)
+  }
+  run.end()
+
+})
+
+bench( `MBR.parse(syslinux) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'syslinux.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.parse(buffer)
+  }
+  run.end()
+
+})
+
+bench( `MBR.parse(rpi) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, '..', 'test', 'data', 'rpi.bin' )
+  var buffer = fs.readFileSync( filename )
+  var mbr = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    mbr = MBR.parse(buffer)
+  }
+  run.end()
+
+})
