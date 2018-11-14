ec22b3dcf01293f97dac0341b6ba18739b2a4fe0
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-11-14 20:46:09 +0100

    test: Update inspect.js

--- test/inspect.js
+++ test/inspect.js
@@ -1,9 +1,17 @@
 var util = require( 'util' )
-var options = {
+
+function inspect( value ) {
+  return util.inspect( value, inspect.options )
+}
+
+inspect.options = {
   depth: null,
   colors: process.stdout.isTTY,
 }
 
-module.exports = function inspect( value ) {
-  return util.inspect( value, options )
+inspect.log = function( value ) {
+  process.stdout.write( inspect( value ) )
+  process.stdout.write( '\n' )
 }
+
+module.exports = inspect
